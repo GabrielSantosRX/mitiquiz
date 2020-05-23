94c9ef17e37bfb2a8cd38b00547ab80705039995
Gabriel <gabrielsantos.info@gmail.com>
2020-05-23 17:02:06 -0400

    Botton Menu Bar

---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // strings
+  static const String appName = 'Mitiquiz';
+
+  // Screens
+  static const String dashboardScreen = '/';
+
+  // colors
+  static Color colorPrimary = Colors.redAccent[400];
+  static const Color colorRedVine = Color.fromRGBO(194, 32, 88, 100);
+  static const Color colorGrayLight = Color.fromRGBO(130, 133, 140, 100);
+  static const Color colorGray = Color.fromRGBO(30, 31, 35, 100);
+  static const Color colorGrayDark = Color.fromRGBO(39, 41, 49, 100);
+
+  // region LocalCacha
+  static const hiveBox = 'MITIQUIZ_BOX';
+}
---
+++ lib/injection_container.dart
@@ -0,0 +1,25 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:mitiquiz/core/constants.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:mitiquiz/core/connectivity.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:dio/dio.dart';
+
+final GetIt getIt = GetIt.instance;
+
+Future<void> init() async {
+  //! Core
+  getIt.registerLazySingleton<IConnectivity>(() => Connectivity(getIt()));
+
+  //! External
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  final box = await Hive.openBox(Constants.hiveBox);
+  getIt.registerLazySingleton(() => box);
+  getIt.registerLazySingleton(() => Dio());
+  getIt.registerLazySingleton(() => DataConnectionChecker());
+
+  //! Features
+  // Controllers
+}
